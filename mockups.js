ec70cad331d2011fe3d162458ede4982ad96e37c
Stolbukhin Maksim <maxstbn@gmail.com>
2014-11-16 19:29:19 +0300

    сосздал моку

--- mockups.js
+++ mockups.js
@@ -1,3 +1,36 @@
 exports.filters = {
-	"brands" : "Бренды"
+	forms: [
+		{
+			name: "Бренды",
+			system_name: "brands",
+			field_type: "checkbox",
+			values: [
+				{
+					id: 1,
+					label: "Samsung",
+					sum: 22 
+				},
+				{
+					id: 2,
+					label: "Explay",
+					sum: 56
+				},
+				{
+					id: 3,
+					label: "Fly",
+					sum: 32
+				},
+				{
+					id: 4,
+					label: "GiNZZU",
+					sum: 1
+				},
+				{
+					id: 5,
+					label: "Philips",
+					sum: 5
+				}
+			]
+		}
+	]
 };
